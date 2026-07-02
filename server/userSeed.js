3811ee85e1c9e207bfe1d3449b73902602bafbf5
Md. Robiul Islam <robiulislam31122002@gmail.com>
2026-07-02 23:17:16 +0600

    DatabaseChange

--- server/userSeed.js
+++ server/userSeed.js
@@ -1,15 +1,12 @@
-// ১. সবার আগে dotenv লোড করতে হবে
 import dotenv from 'dotenv';
 dotenv.config(); 
 
-// ২. এরপর বাকি ইম্পোর্টগুলো করতে হবে
 import User from './models/User.js';
 import bcrypt from 'bcrypt';
 import connectToDatabase from './db/db.js';
 
 const userRegister = async () => {
   try {
-    // চেক করার জন্য: URL ঠিকমতো লোড হচ্ছে কি না
     if (!process.env.MONGODB_URL) {
       console.error("❌ MONGODB_URL is missing! Check your .env file location.");
       return;
@@ -19,7 +16,6 @@ const userRegister = async () => {
 
     const hashPassword = await bcrypt.hash("authority", 10);
 
-    // অবজেক্টটি সরাসরি নিউ ইউজার হিসেবে ডিক্লেয়ার করা
     const newUser = new User({
       name: "authority",
       email: "authority@gmail.com",
