e61a67a2528b7426a890fde5c33ad9526f87ebf3
Robiul <robiulislam31122002@gmail.com>
2026-07-31 12:22:23 +0600

    Change button text from 'Submit' to 'Update'

--- Frontend/src/components/student/StudentProfileUpdate.jsx
+++ Frontend/src/components/student/StudentProfileUpdate.jsx
@@ -167,7 +167,7 @@ const StudentProfileUpdate = () => {
           </div>
 
           <button type="submit" className="submit-btn">
-            Submit
+            Update
           </button>
 
         </form>
